130697946040b2232ee0690c8e70f48f622001ce
KMobin555 <kamrujjamanmobin123@gmail.com>
2025-06-19 23:45:17 +0600

    fisherman

--- flip_columns.cpp
+++ flip_columns.cpp
@@ -5,13 +5,34 @@
 
 using namespace std;
 
-int n,m,k;
+int n,m,k,ans;
 
+void check(int col,int k, vector<vector<int>> s)
+{
+    if(k==0)
+    {
+        for(int i=0;i<n;i++)
+        {
+            int cnt = 0;
+            for(int j=0;j<m;j++) 
+                if(s[i][j]==1) cnt++;
+            if(cnt)
+        }
+    }
+}
 
 void solve(int cs)
 {
     cin >> n >> m >> k;
-    vector<int> 
+    vector<vector<int>> s(n,vector<int>(m));
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++) cin >> s[i][j];
+    } 
+    ans = 0;
+    check(0,k,s);
+
+    cout << ans << endl;
 }
 
 int main()
