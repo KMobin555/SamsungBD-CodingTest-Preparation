a52f34de08a39488682b98ccb8af42a9013ceb48
KMobin555 <kamrujjamanmobin123@gmail.com>
2025-06-22 10:59:38 +0600

    aeroplane bombing v2

--- Aeroplane_Bombing_v2.cpp
+++ Aeroplane_Bombing_v2.cpp
@@ -1,47 +1,64 @@
 #include<iostream>
 #include<cstdio>
 #include<vector>
+#include<climits>
+#include<algorithm>
 
 using namespace std;
 
-int n,ans;
-vector<vector<int>> s;
 
-void back(int i,int j,int canBomb)
+int back(int i,int j,bool doBomb, vector<vector<int>> s,int row_cnt)
 {
-    if(canBomb)
-    {
+    if(i<0 || j<0 || j>=5) return 0;
 
-    }
-    else
+    if(s[i][j]==2)
     {
-        
+        if(row_cnt>5 && doBomb) return 0;
+        else if(!doBomb) doBomb = true;
     }
+
+    int val = 0;
+    for(int p=-1;p<=1;p++)
+    {
+        if(doBomb)
+        {
+            val=max(val,back(i-1,j+p,true,s,row_cnt+1));
+        }
+        else
+        {
+            val = max(val,back(i-1,j+p,false,s,1));
+            val = max(val,back(i-1,j+p,true,s,row_cnt+1));
+        }
+    }
+
+    return (s[i][j]==1)?1+val:val;
 }
 
 void solve(int cs)
 {
+    int n;
     cin >> n;
-    s.resize(n+1,vector<int>(5,0));
+    
+    vector<vector<int>> s(n+1,vector<int>(5,0));
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<5;j++) cin >> s[i][j];
     }
     
-    ans = 99999999;
-
-    back(n,2,1);
+    int ans = back(n,2,false,s,0);
 
     cout << "#" << cs << " ";
     cout << ans << "\n";
 }
 
-int main()
+void io()
 {
-    #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    #endif
+}
+int main()
+{
+    // io();
 
     int t = 1, cs = 1;
     cin >> t;
