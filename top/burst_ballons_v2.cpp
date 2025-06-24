1d72daac411e35de6b30059da21b38272e2a4a96
KMobin555 <kamrujjamanmobin123@gmail.com>
2025-06-24 20:42:30 +0600

    ballons

---
+++ top/burst_ballons_v2.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<cstring>
+#include<climits>
+using namespace std;
+
+int n,dp[111][111];
+
+int back(int l,int r,vector<int> &s, int is_first)
+{
+    if(l>r) return 0;
+    if(dp[l][r]!=-1) return dp[l][r];
+
+    int val = INT_MIN;
+    for(int i=l;i<=r;i++)
+    {
+        if(is_first)
+        {
+            val = max(val, back(l,i-1,s,0)+back(i+1,r,s,0)+s[i]);
+        }
+        else
+        {
+            val = max(val, back(l,i-1,s,0)+back(i+1,r,s,0)+(s[l-1]*s[r+1]));
+        }
+    }
+    return dp[l][r]=val;
+}
+void solve(int cs)
+{
+    cin >> n;
+    vector<int> s;
+    s.push_back(1);
+    for(int i=0;i<n;i++) 
+    {
+        int x;cin >> x;
+        s.push_back(x);
+    }
+    s.push_back(1);
+
+    memset(dp,-1,sizeof(dp));
+
+    int ans = back(1,n,s,1);
+
+    cout << ans << endl;
+}
+
+void io()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+
+int main()
+{
+
+    // io();
+    
+    int t = 1, cs = 1;
+    // cin >> t;
+    while(t--) solve(cs++);
+
+    return 0;
+}
